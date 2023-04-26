38957c0436d8b27b5166604fa9f8d2203d7b9c42
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-04-26 14:59:52 +0300

    awesome first commit

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,22 +1,51 @@
 import React, { useState } from "react";
-import { spicyFoods, getNewRandomSpicyFood } from "../data";
+// import { spicyFoods, getNewRandomSpicyFood } from "../data"
+
+// importing newspicyfoods in the component module
+import { spicyFoods, getNewRandomSpicyFood, newSpicyFoods } from "../data";
+
+
 
 function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+  // const [foods, setFoods] = useState(spicyFoods);
 
-  function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
-  }
+  // updating the initial food state from the above
+  // using spread operator to update the current state
+  const [foods, setFoods] = useState([...spicyFoods, ...newSpicyFoods]);
 
+  // defining handleAddFood function 
+  function handleAddFood(){
+    // wrapping new food in an array to iterate over
+    const newFood = [getNewRandomSpicyFood()];
+    const newFoodArray =[...foods, ...newFood];
+    setFoods(newFoodArray);
+  };
+
+
+
+//  commenting the code here to try something new with the code but still valid
+  // function handleAddFood() {
+  //   const newFood = getNewRandomSpicyFood();
+  //   console.log(newFood);
+  // }
+
+  // will be updating my foodList variable to only map over foods,newspicyfoods already exists
   const foodList = foods.map((food) => (
     <li key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+
+//since am not adding new foods to the component, will be removing the Add New Food for while
   return (
     <div>
+      {/* removing this return statement only complicates the code from adding the food  */}
+      {/* only creates room for displaying all the food but the console complains about the process */}
+      {/* <button onClick={handleAddFood}>Add New Food</button> */}
+
+{/* the below statement only add the add new food button the screen */}
+{/* the add button is helping me communicate with the Dom from the browser to see whats being rendered */}
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
@@ -24,3 +53,10 @@ function SpicyFoodList() {
 }
 
 export default SpicyFoodList;
+
+
+
+
+// the code is able to add fooditems in the menu bar of the browser
+// 
+// 
--- src/data/index.js
+++ src/data/index.js
@@ -6,6 +6,9 @@ const spicyFoods = [
 let nextId = 3;
 
 const newSpicyFoods = [
+  // adding extra food items to display
+  {name: "Matoke Nyama", cusisine:  "African", heatLevel: 8},
+  {name: "Githeri Chapati", cuisine: "Kenyan", heatLevel: 6},
   { name: "Green Curry", cuisine: "Thai", heatLevel: 9 },
   { name: "Enchiladas", cuisine: "Mexican", heatLevel: 2 },
   { name: "5 Alarm Chili", cuisine: "American", heatLevel: 5 },
@@ -19,5 +22,6 @@ function getNewRandomSpicyFood() {
   nextId++;
   return newSpicyFood;
 }
+export { spicyFoods, getNewRandomSpicyFood, newSpicyFoods };
 
-export { spicyFoods, getNewRandomSpicyFood };
+// export { spicyFoods, getNewRandomSpicyFood };
